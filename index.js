98b8860110219ea287ec2cbc42bd8088bf1cffc0
Johnson Sudré <johnsonsudre@gmail.com>
2021-05-07 19:30:15 -0300

    'Init'

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const { read } = require("fs");
+const path = require("path");
+const { convert, toMoney } = require("./lib/convert");
+
+process.env.PORT = 3000;
+
+// CONFIG
+const app = express();
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+// ...definir pasta ./public
+app.use(express.static(path.join(__dirname, "public")));
+
+// PORT
+app.listen(process.env.PORT, (err) => {
+  if (err) {
+    console.log("Não foi possível comunicar com o servidor. Erro: ", err);
+  } else {
+    console.log("convert my money está online em", process.env.PORT);
+  }
+});
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/quotation", (req, res) => {
+  const { query } = req;
+  const { quotation, quantity } = query;
+  if (quotation && quantity) {
+    const convertion = convert(quotation, quantity);
+    console.log(query);
+    res.render("quotation", {
+      error:false,
+      quotation: toMoney(quotation),
+      quantity: toMoney(quantity),
+      convertion: toMoney(convertion),
+    });
+  } else {
+    res.render('quotation', {
+      error: 'Valor inválido.'
+    })
+  }
+});
